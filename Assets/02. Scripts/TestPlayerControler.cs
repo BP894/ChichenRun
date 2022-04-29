904d853fba8698764381ff6a9fb50e1d64bec1e9
Bumzin Yoo <bluerain894@gmail.com>
2022-04-29 23:39:16 +0900

    01. 플레이어 애니메이션 재설정

--- Assets/02. Scripts/TestPlayerControler.cs
+++ Assets/02. Scripts/TestPlayerControler.cs
@@ -5,18 +5,47 @@ using UnityEngine;
 public class TestPlayerControler : MonoBehaviour
 {
     public float movementSpeed = 5f;
+    public float jumpPower = 5f;
+    private bool isJump = false;
+
     public SpawnManager spawnManager;
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
-        float hMovement = Input.GetAxis("Horizontal") * movementSpeed;
-        float vMovement = Input.GetAxis("Vertical") * movementSpeed / 2;
+        if (!isJump)
+        {
+            Move();
+            Rotate();
+        }
+        else
+        {
+            transform.Translate(new Vector3(0, 0, movementSpeed) * Time.deltaTime);
+        }
 
-        if (Input.GetAxis("Horizontal") > 0)
+        Jump();
+    }
+    private void Move()
+    {
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed;
+
+        transform.Translate(new Vector3(hMovement, 0, movementSpeed) * Time.deltaTime);
+    }
+
+    private void Rotate()
+    {
+        float vRotation = Input.GetAxis("Horizontal");
+
+        if (vRotation > 0)
         {
             transform.GetChild(0).localRotation = Quaternion.Euler(new Vector3(0, 45, 0));
         }
-        else if (Input.GetAxis("Horizontal") < 0)
+        else if (vRotation < 0)
         {
             transform.GetChild(0).localRotation = Quaternion.Euler(new Vector3(0, -45, 0));
         }
@@ -24,12 +53,34 @@ public class TestPlayerControler : MonoBehaviour
         {
             transform.GetChild(0).localRotation = Quaternion.Euler(Vector3.zero);
         }
-
-        transform.Translate(new Vector3(hMovement, 0, movementSpeed) * Time.deltaTime);
     }
 
+    private void Jump()
+    {
+      
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isJump)
+            {
+                isJump = true;
+                transform.GetChild(0).localRotation = Quaternion.Euler(Vector3.zero);
+                rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
     private void OnTriggerExit(Collider other)
     {
         spawnManager.SpawnTriggerEntered();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("GROUND"))
+        {
+            isJump = false;
+        }
     }
 }
